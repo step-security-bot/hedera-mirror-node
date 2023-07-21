81e6a04ce5bab37780e2a4a41351a849bb0bfb5a
Ivan Ivanov <56980328+0xivanov@users.noreply.github.com>
2023-07-21 13:47:03 -0700

    HIP-584: Copy ApprovePrecompile (#6446)
    We need default implementations for precompiles related to approve logic, so we should copy needed logic from hedera-services into hedera-mirror-web3.
    
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- hedera-mirror-web3/src/main/java/com/hedera/services/store/contracts/precompile/SyntheticTxnFactory.java
+++ hedera-mirror-web3/src/main/java/com/hedera/services/store/contracts/precompile/SyntheticTxnFactory.java
@@ -36,7 +36,9 @@ import com.hedera.services.store.contracts.precompile.codec.UnpauseWrapper;
 import com.hedera.services.store.contracts.precompile.codec.WipeWrapper;
 import com.hedera.services.store.models.Id;
 import com.hedera.services.utils.EntityIdUtils;
+import com.hedera.services.store.models.Id;
 import com.hederahashgraph.api.proto.java.AccountID;
+import com.hederahashgraph.api.proto.java.CryptoApproveAllowanceTransactionBody;
 import com.hederahashgraph.api.proto.java.ContractCallTransactionBody;
 import com.hederahashgraph.api.proto.java.ContractID;
 import com.hederahashgraph.api.proto.java.CryptoApproveAllowanceTransactionBody;
@@ -71,6 +73,10 @@ import java.util.Objects;
 import lombok.NonNull;
 import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.datatypes.Address;
+import jakarta.annotation.Nullable;
+import java.util.List;
+import java.util.Objects;
+import lombok.NonNull;
 
 public class SyntheticTxnFactory {
     public static final String HTS_PRECOMPILED_CONTRACT_ADDRESS = "0x167";
